8229c061a2b2d9f34930cd2e12e3755c78fd4475
Phalin Pancholi <41735720+Phillll99@users.noreply.github.com>
2019-10-09 16:17:40 +0530

    Returns list of strings in a wave form

---
+++ Mexicon_wave.py
@@ -0,0 +1,15 @@
+def wave(str):
+    temp_list = []
+    n = len(str)
+    for i in range(n):
+        if str[i] == " ":
+            continue
+        s = str[0:i] + str[i].upper() + str[i+1:n]
+        temp_list.append(s)
+    return temp_list
+
+s = input()
+wave(s)
+#def wave(str):
+#    return [str[:i] + str[i].upper() + str[i+1:] for i in
+#    range(len(str)) if str[i].isalpha()]
