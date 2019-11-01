32eba995c4a6b16fdf1779b92f1e7b2313052e56
Phalin Pancholi <41735720+P-H-Pancholi@users.noreply.github.com>
2019-11-01 19:34:40 +0530

    Update to find 2's complement for negative no.

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -5,20 +5,31 @@ Created on Fri Jun 21 21:40:58 2019
 @author: Phalin Pancholi
 """
 
-num = int(input("Enter the number: "))
-if num < 0:
-    IsNeg = True
-    num = abs(num)
-else:
-    IsNeg = False
-result = " "
-if num == 0:
-    result = '0'
-while num > 0:
-    result = result + str(num % 2)
-    num = num//2
-if IsNeg:
-    result = "-" + result
-print(result)
-    
+def inversebit(s):
+    if s == '0':
+        return '1'
+    else:
+        return '0'
+def decimaltobinary(n):
+    s = ''
+    if n == 0:
+        return '0'
+    if n > 0:
+        while n != 0:
+            s = s + str(n % 2)
+            n = n // 2
+        return s[::-1]
+    if n < 0:
+        s = decimaltobinary(abs(n))
+        Bflag = False
+        temp = []
+        for char in s[::-1]:
+            if Bflag:
+                temp.append(inversebit(char))
+            else:
+                temp.append(char)
+                if char == '1':
+                    Bflag = True
+        s = "".join(temp)
+        return s[::-1]
     
